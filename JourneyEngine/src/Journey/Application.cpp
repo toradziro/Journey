42b7744a7cef80e441654605d645f28eea46b4d6
ehillman <toradziro@gmail.com>
2024-08-08 23:45:47 +0200

    Wanna add interpolation in colors

--- JourneyEngine/src/Journey/Application.cpp
+++ JourneyEngine/src/Journey/Application.cpp
@@ -65,8 +65,8 @@ Application::Application()
 	float vertices[3 * 7] =
 	{
 		-0.5f, -0.5f, 0.0f,0.2f, 0.5f, 0.0f, 1.0f,
-		0.5f, -0.5f, 0.0f, 0.2f, 0.5f, 0.0f, 1.0f,
-		0.0f, 0.5f, 0.0f,  0.2f, 0.5f, 0.0f, 1.0f
+		0.5f, -0.5f, 0.0f, 0.2f, 0.0f, 0.5f, 1.0f,
+		0.0f, 0.5f, 0.0f,  0.9f, 0.2f, 0.0f, 1.0f
 	};
 
 	//-- Vertex buffer
