1e3c8113d893e184f751a3a1e1e95455573b9862
ehillman <toradziro@gmail.com>
2024-08-10 23:47:22 +0200

    Added return vb & ib from va

--- JourneyEngine/src/Journey/Renderer/OpenGL/OpenGLVertexArray.h
+++ JourneyEngine/src/Journey/Renderer/OpenGL/OpenGLVertexArray.h
@@ -17,6 +17,9 @@ public:
 	virtual void addVertexBuffer(const std::shared_ptr<VertexBuffer>& vertexBuffer) override;
 	virtual void setIndexBuffer(const std::shared_ptr<IndexBuffer>& IndexBuffer) override;
 
+	virtual const std::vector<std::shared_ptr<VertexBuffer>>& vertexBuffers() const override { return m_vertexBuffers; }
+	virtual const std::shared_ptr<IndexBuffer>& indexBuffer() const override { return m_indexBuffer; }
+
 private:
 	std::vector<std::shared_ptr<VertexBuffer>>	m_vertexBuffers;
 	std::shared_ptr<IndexBuffer>				m_indexBuffer;
--- JourneyEngine/src/Journey/Renderer/VertexArray.h
+++ JourneyEngine/src/Journey/Renderer/VertexArray.h
@@ -16,6 +16,9 @@ public:
 	virtual void addVertexBuffer(const std::shared_ptr<VertexBuffer>& vertexBuffer) = 0;
 	virtual void setIndexBuffer(const std::shared_ptr<IndexBuffer>& IndexBuffer) = 0;
 
+	virtual const std::vector<std::shared_ptr<VertexBuffer>>& vertexBuffers() const = 0;
+	virtual const std::shared_ptr<IndexBuffer>& indexBuffer() const = 0;
+
 	static VertexArray* create();
 };
 
